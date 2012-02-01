a9d414334c8d935291c7e7e4303b92942a2b82db
Sharlene Ku <atchie_ku11@yahoo.com>
2012-02-01 22:16:00 +0800

    second commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>Google's Bouncing Balls | HTML5 Canvas | Rob Hawkes</title>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="css/main.css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
+	<body>
+		<canvas id="c" width="400" height="400"></canvas>
+		<p id="attribution">Made by Sharlene Ku –  <a href="https://github.com/robhawkes/google-bouncing-balls.git">Source Code by Rob Hawkes</a></p>
+	</body>
+</html>
